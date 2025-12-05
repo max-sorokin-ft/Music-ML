e7c22eff99f572887061d006b00b78a629e13151
max-sorokin-ft <maxsorokin.ft@gmail.com>
2025-12-05 20:21:23 +0000

    Ingestion Flow

---
+++ flows/ingestion_flow.py
@@ -0,0 +1,26 @@
+import sys
+import subprocess
+from prefect import flow, task
+
+@task
+def run_script(script_path, flags):
+    command = [sys.executable, "-m", script_path] + flags
+    subprocess.run(command, check=True)
+
+@flow(name="ingestion_flow", log_prints=True)
+def ingestion_flow(page_number: int, batch_number: int):
+    p = str(page_number)
+    b = str(batch_number)
+
+    run_script("ingestion.get_artists", ["--page_number", p, "--batch_number", b, "--num", "1"])
+    run_script("ingestion.get_albums", ["--page_number", p, "--batch_number", b, "--num", "1"])
+    run_script("ingestion.get_songs", ["--page_number", p, "--batch_number", b, "--num", "2"])
+    run_script("ingestion.get_isrc_and_pop", ["--page_number", p, "--batch_number", b, "--num", "3"])
+    run_script("ingestion.group_songs", ["--page_number", p, "--batch_number", b])
+    run_script("ingestion.get_streams", ["--page_number", p, "--batch_number", b, "--num", "1"])
+    run_script("ingestion.create_parquet", ["--page_number", p, "--batch_number", b])
+    run_script("ingestion.insert_db", ["--page_number", p, "--batch_number", b])
+
+
+if __name__ == "__main__":
+    ingestion_flow(page_number=1, batch_number=1)
